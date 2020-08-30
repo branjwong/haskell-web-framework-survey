76cdd445809cc65ef6c99cb00f49447cda3b470d
Brandon J Wong <branjwong@gmail.com>
2020-08-30 14:50:43 -0700

    add happstack

--- happstack-test/README.md
+++ happstack-test/README.md
@@ -1,5 +1,17 @@
 # happstack
 
-## likes
+## how to run
 
-## dislikes
+1. terminal: run `stack run app/Main.hs`
+2. browser: navigate to 0.0.0.0:8000
+
+## assessment
+
+### likes
+
+- Hackage uses it
+- happstack lite is light
+- easier to get started
+- can move to normal happstack when we need more functionality and more comfortable
+
+### dislikes
--- happstack-test/src/Lib.hs
+++ happstack-test/src/Lib.hs
@@ -1,6 +1,32 @@
 module Lib
-    ( someFunc
-    ) where
+  ( runner,
+  )
+where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Applicative (optional, (<$>))
+import Data.ByteString.Char8 as C
+import Data.ByteString.Lazy.Char8 as L
+import Data.Maybe (fromMaybe)
+import Happstack.Lite
+
+runner :: IO ()
+runner = serve config myApp
+
+config :: Maybe ServerConfig
+config =
+  Just
+    ( ServerConfig
+        { port = 8000,
+          ramQuota = 1 * 10 ^ 6,
+          diskQuota = 20 * 10 ^ 6,
+          tmpDir = "/tmp/"
+        }
+    )
+
+myApp :: ServerPart Response
+myApp =
+  ok
+    ( toResponseBS
+        (C.pack "text/plain")
+        (L.pack "hello, world")
+    )
